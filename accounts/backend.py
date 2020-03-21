aab9598f0741dbd095e2ec6bb846160def5d7e8e
Patrick564 <pvilchez794@gmail.com>
2020-03-21 11:39:25 -0500

    before first migrate

---
+++ accounts/backend.py
@@ -0,0 +1,23 @@
+from django.contrib.auth.backends import BaseBackend
+
+from .models import User
+
+
+class AuthBackend(BaseBackend):
+
+    def authenticate(self, request, email, password):
+        try:
+            user = User.objects.get(email=email)
+
+            if user.check_password(password):
+                return user
+
+        except User.DoesNotExist:
+            return None
+
+    def get_user(self, user_id):
+        try:
+            return User.objects.get(pk=user_id)
+
+        except User.DoesNotExist:
+            return None
---
+++ accounts/models.py
@@ -0,0 +1,65 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser, UserManager
+
+
+class AccountUserManager(UserManager):
+
+    def _create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError('Email is required')
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_user(self, email=None, password=None, **extra_fields):
+        return self._create_user(
+            email=email,
+            password=password,
+            **extra_fields
+        )
+
+    def create_superuser(self, email=None, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        return self._create_user(
+            email=email,
+            password=password,
+            **extra_fields
+        )
+
+
+class User(AbstractUser):
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+    GENDER_CHOICES = (
+        ('F', 'Female'),
+        ('M', 'Male'),
+    )
+
+    username = models.CharField(blank=True, unique=False, max_length=50)
+    email = models.EmailField(required=True, unique=True, max_length=254)
+    date_of_birth = models.DateField(auto_now=False, auto_now_add=False)
+    gender = models.CharField(choices=GENDER_CHOICES, max_length=15)
+    phone_number = models.PhoneNumberField()
+
+    objects = AccountUserManager()
+
+    def __str__(self):
+        return self.email
+
+
+class ContactsFields(models.Model):
+    email = models.ForeignKey(
+        'User',
+        on_delete=models.CASCADE
+    )
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    phone_number = models.PhoneNumberField()
+    contact_email = models.EmailField(max_length=254)
+    date_of_birth = models.DateField(auto_now=False, auto_now_add=False)

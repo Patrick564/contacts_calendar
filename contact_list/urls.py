433d7f8deeaea1ee4d056ea613dc69ee218f3cd5
Patrick564 <pvilchez794@gmail.com>
2020-05-04 15:06:00 -0500

    templates in app contact_list add

--- contact_list/urls.py
+++ contact_list/urls.py
@@ -5,5 +5,21 @@ from . import views
 app_name = 'contact_list'
 
 urlpatterns = [
+    # Index
     path('', views.index, name='index'),
+
+    # Add
+    path('add/', views.add, name='add'),
+
+    # Update
+    path('update/', views.update, name='update'),
+
+    # Favorite
+    # path('favorite/', name='favorite'),
+
+    # Filed
+    # path('filed/', name='filed'),
+
+    # Download
+    # path('download/', name='download'),
 ]

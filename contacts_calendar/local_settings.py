53c402edfdc853e44566dfb645ded01bf9e46925
Patrick564 <pvilchez794@gmail.com>
2020-06-01 17:57:35 -0500

    local settings added

---
+++ contacts_calendar/local_settings.py
@@ -0,0 +1,13 @@
+secret_key = 'wet$tem&_(@y+&@^(u&m@*&+g%29(*6#s*6#b3#&z^86ioy9qe'
+
+email_host_user = 'pvilchez794@gmail.com'
+email_host_password = 'phnalsfiolagmmkr'
+
+db_mysql_conf = {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'contacts_calendar',
+        'USER': 'root',
+        'PASSWORD': '12345',
+        'HOST': 'localhost',
+        'PORT': '3306',
+    }

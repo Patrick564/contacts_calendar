d486fc9c9df27060aeeea0732a43743aca1d6f38
Patrick <pvilchez794@gmail.com>
2020-11-29 11:28:28 -0500

    Staticfiles dirs deleted from settings base

--- contacts_calendar/settings/base.py
+++ contacts_calendar/settings/base.py
@@ -136,7 +136,6 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'))
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 TAILWIND_APP_NAME = 'theme'

abaf385ff6f4e6aaac6b9e2f9c39f1558374f965
Patrick <pvilchez794@gmail.com>
2020-11-29 12:10:38 -0500

    Allowed hosts updated

--- contacts_calendar/settings/base.py
+++ contacts_calendar/settings/base.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*', '.herokuapp.com']
 
 AUTH_USER_MODEL = 'accounts.User'
 

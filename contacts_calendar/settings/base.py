72db61e228c9972ef2d639e52090749806a36623
Patrick <pvilchez794@gmail.com>
2020-11-29 22:18:53 -0500

    Icon page added

--- contacts_calendar/settings/base.py
+++ contacts_calendar/settings/base.py
@@ -115,10 +115,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-# STATICFILES_DIRS = (
-#     os.path.join(BASE_DIR, 'static'),
-# )
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'  # noqa
 
 # Redirect conf
 LOGIN_URL = '/accounts/login/'

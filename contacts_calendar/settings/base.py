73e9d7a365bdd3cbe2dbef015276e834ff488d4c
Patrick <pvilchez794@gmail.com>
2020-11-29 21:50:44 -0500

    Runtime python version changed

--- contacts_calendar/settings/base.py
+++ contacts_calendar/settings/base.py
@@ -10,13 +10,13 @@ else:
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG_SETTING')
+DEBUG = False
 
 ALLOWED_HOSTS = ['*', '.herokuapp.com', 'contacts-calendar.herokuapp.com']
 
@@ -118,7 +118,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'  # noqa
 
 # Redirect conf
 LOGIN_URL = '/accounts/login/'

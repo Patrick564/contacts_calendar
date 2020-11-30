e91a784422105f8741ca45e4c855ffee2186dee9
Patrick <pvilchez794@gmail.com>
2020-11-29 21:29:58 -0500

    Debug setting changed

--- contacts_calendar/settings/base.py
+++ contacts_calendar/settings/base.py
@@ -16,7 +16,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.getenv('DEBUG_SETTING')
 
 ALLOWED_HOSTS = ['*', '.herokuapp.com', 'contacts-calendar.herokuapp.com']
 

b2458a17e497f1efbd59451f37d8cb89fa4e0bdd
Patrick Vilchez <pvilchez794@gmail.com>
2022-04-12 14:19:52 -0500

    Static root changed in production setting

--- contacts_calendar/settings/production.py
+++ contacts_calendar/settings/production.py
@@ -26,7 +26,7 @@ if RENDER_EXTERNAL_HOSTNAME:
 
 # Static files
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATIC_ROOT = BASE_DIR + '/staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Database
 DATABASES = {

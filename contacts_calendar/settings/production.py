6017063cb3e56788b15499ee1974b27894492f16
Patrick <pvilchez794@gmail.com>
2023-02-28 18:30:33 -0500

    Settings update

--- contacts_calendar/settings/production.py
+++ contacts_calendar/settings/production.py
@@ -28,7 +28,7 @@ if RENDER_EXTERNAL_HOSTNAME:
 
 # Static files
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Database
 DATABASES = {
